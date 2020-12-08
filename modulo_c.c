56162edc72931176d8d3d5b0a833f2d64fd1fd69
João <jpdelgado2001@gmail.com>
2020-12-08 16:13:21 +0000

    .c e .h

--- modulo_c.c
+++ modulo_c.c
@@ -1,81 +1,77 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <string.h>
-#define SIMBOLOS 256 
+ #include "modulo_c.h"
+
 int nBlocks(FILE *cod){
-    int n_blocks = 0;
-    fseek (cod, 3,SEEK_SET);
     char c;
-    while ((c = fgetc (cod)) != 64){
-        n_blocks = n_blocks * 10 + (c - 48);
+    int n_blocks = 0;
+    fseek(cod, 3, SEEK_SET);
+    while((c = fgetc(cod)) != 64){
+        n_blocks = n_blocks * 10 + (c-48);
     }
     return n_blocks;
 }
-void printMatriz(int (*matrix)[SIMBOLOS], int numberOfLines , int numberColumns){
-    int row, columns;
-    for (row=0; row<numberOfLines; row++)
-    {
-        for(columns=0; columns<numberColumns; columns++)
-        {
-            printf("%d     ", matrix[row][columns]);
-        }
-        printf("\n");
-    }
-}
-void importCode( char *codFile){
-    FILE *cod;
-    int n_blocks,blocos,simbolo = 0;
-    char c,ant;
-    cod = fopen(codFile,"rb");
-    if(!cod) printf("nao existe"); 
-    else {
-        n_blocks = nBlocks(cod);
-        printf("%d\n",n_blocks);
-        int matrizCodigos[n_blocks][256];
-        while((c = fgetc(cod)) != 64);
-        blocos = 1;
-        ant = ';';
+
+void importCode(char* codFile){
+    FILE *cod = fopen (codFile, "rb");
+    if (!cod) printf ("Error!\n");
+    else{
+        printf("Sucess!\n");
         
-        while(blocos <= n_blocks){
+        int n_blocks = nBlocks(cod);
+        printf ("Número de Blocos : %d\n", n_blocks);
+        char c;
+
+        
+        int matrizCodigos [n_blocks][SIMBOLOS];
+        int blocos = 0;
+        int simbolos = 0;
+        char ant = ';';
+        int codigo;
+        
+        while(blocos < n_blocks){
+
+            simbolos = 0;
+            //avancar ate ao inicio do bloco
+            while((c = fgetc(cod)) != '@');
             c = fgetc(cod);
-            //printf(" (%c) ",c);
-            if(c == ';' && ant == ';'){ 
-                matrizCodigos[blocos - 1][simbolo] = -1;
-                //printf("%d 1ºif ",matrizCodigos[blocos -1][simbolo]);
-                simbolo++;
-                ant = c;
-            }
-            else if(c == '@'){
-                if(ant == ';') matrizCodigos[blocos - 1][simbolo] = -1;
-                for(c = fgetc(cod) ; c != EOF && c != 64 ; c = fgetc(cod)); // avançar para o proximo bloco
-                blocos++;
-                simbolo = 0;
-                ant = ';';
-            }
-            else{
-                matrizCodigos[blocos - 1][simbolo] = 0;
-                while(c != 59 && c != 64){
-                    matrizCodigos[blocos - 1][simbolo] = matrizCodigos[blocos -1][simbolo] * 10 + ( c - 48 );
-                    c = fgetc(cod);
+
+            while(c != '@'){
+                if(c == ';'){
+                    matrizCodigos[blocos][simbolos] = -1;    
+                }else{
+                    codigo = 0;
+                    while(c != ';' && c != '@'){
+                        codigo *= 10;
+                        codigo += (c-48);
+                        c = fgetc(cod);
+                    }
+                    if(c == '@'){
+                        fseek(cod, -1, SEEK_CUR);
+                    }
+                    matrizCodigos[blocos][simbolos] = codigo;
                 }
-                printf("%d ",matrizCodigos[blocos - 1][simbolo]); 
-                simbolo++;
-                ant = c;
+                simbolos++;
+                c = fgetc(cod);
             }
+            if (simbolos < 256){
+                matrizCodigos[blocos][simbolos] = -1;
+            }
+            blocos++;
         }
-        printMatriz(matrizCodigos, n_blocks,SIMBOLOS);  
-    }
-    
-    fclose(cod);
+
+        for(int i = 0; i < n_blocks; i++){
+            printf("simbolos: ");
+            for(int j = 0; j < SIMBOLOS; j++){
+                printf("%d, ", matrizCodigos[i][j]);            
+            }
+            printf("\n");
+        }
+    } 
 }
 
-
-int main(int argc , char *argv[]){
-    int size = strlen(argv[1]) + 4;
+int main(int argc, char *argv[]){
+    int size = strlen(argv[1]);
     char codFile[size];
-    strcpy(codFile,argv[1]);
-    strcat(codFile,".cod"); 
+    strcpy (codFile,argv[1]);
+    strcat(codFile,".cod");
     importCode(codFile);
-    return 0;
 }
- 
---
+++ modulo_c.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIMBOLOS 256
+
+
+int nBlocks(FILE *);
+void importCode(char*);
+int main(int , char **);
